675563987f2e5a0ce4fa540f7f87b1c718cbe8a6
yesiah <hyenhsiang@gmail.com>
2021-05-11 17:40:08 +0800

    Being able to include txt resource file in onedir mode

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # contract-generator-pyside2
 Another contract generator built on PySide2
+
+Build:
+
+`[PYTHONOPTIMIZE=1] pyinstaller --onefile --windowed --clean contract_generator.spec`
--- contract_generator.py
+++ contract_generator.py
@@ -1,17 +1,30 @@
+import os
 import sys
 import pathlib
 from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QMessageBox
 
 import markdown
 from weasyprint import HTML
 
 from contract_generator_ui_model import Ui_MainWindow
 
+# def meipass():
+#     try:
+#         return sys._MEIPASS
+#     except Exception:
+#         return os.path.abspath(".")
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        # If works for onedir mode
+        path = os.path.join(pathlib.Path(sys.executable).parent, "templates/contract_templates/cht/中文契約範本.template")
+        with open(path, 'rb') as f:
+            txt = f.read().decode('UTF-8')
+            QMessageBox.information(self, "Info", txt)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
